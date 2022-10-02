2ef68f92e5b7c18d70c4ca1793b6df90b5d8633b
Driijko <driijko@gmail.com>
2022-10-02 13:41:21 -0400

    Vision page: page entrance.

--- vision/vision.js
+++ vision/vision.js
@@ -15,6 +15,18 @@ window.addEventListener("resize", e => {
   }
 });
 
+window.addEventListener("load", ()=> {
+  const loadingScreen = document.getElementById("loading-screen");
+  loadingScreen.style.opacity = 0;
+  const timerId = setTimeout(()=> {
+    loadingScreen.style.display = "none";
+    const fadeIns = Array.from(document.querySelectorAll(".fade-in"));
+    fadeIns.forEach(element => {
+      element.style.opacity = 1;
+    });
+  }, 3000);
+});
+
 // CONSTANTS and VARIABLES //////////////////////////////////////////////
 const scrollSnapContainer = document.querySelector("#scroll-snap-container");
 const snapPoints = Array.from(
